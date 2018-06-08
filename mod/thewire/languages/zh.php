e0598a0c2328063ddff3cfa41d52710017a2c879
beautylix <beautylix@126.com>
2018-06-08 12:31:10 +0800

    Update zh.php

--- mod/thewire/languages/zh.php
+++ mod/thewire/languages/zh.php
@@ -4,50 +4,50 @@ return array(
 	/**
 	 * Menu items and titles
 	 */
-	'thewire' => "短贴",
-	'thewire:everyone' => "全部帖子",
-	'thewire:user' => "%s'的帖子",
-	'thewire:friends' => "朋友的帖子",
-	'thewire:replying' => "回复到%s (@%s) 他写道",
+	'thewire' => "推文",
+	'thewire:everyone' => "全部推文",
+	'thewire:user' => "%s'的推文",
+	'thewire:friends' => "朋友的推文",
+	'thewire:replying' => "回复%s(@%s)的推文",
 	'thewire:thread' => "主贴",
 	'thewire:charleft' => "剩余的字符",
-	'thewire:tags' => "标签为'%s的短贴'",
-	'thewire:noposts' => "尚无短贴",
-	'item:object:thewire' => "全部帖子",
-	'thewire:by' => '%s的帖子',
-	'thewire:previous:help' => "查看先前的帖子",
-	'thewire:hide:help' => "隐藏先前的帖子",
+	'thewire:tags' => "标签为'%s的推文'",
+	'thewire:noposts' => "尚无推文",
+	'item:object:thewire' => "全部推文",
+	'thewire:by' => '%s的推文',
+	'thewire:previous:help' => "查看先前的推文",
+	'thewire:hide:help' => "隐藏先前的推文",
 
 	/**
 	 * The wire river
 	 */
-	'river:create:object:thewire' => "%s 贴到 %s",
-	'thewire:wire' => '帖文',
+	'river:create:object:thewire' => "%s贴到%s",
+	'thewire:wire' => '推文',
 
 	/**
 	 * Wire widget
 	 */
-	'thewire:widget:desc' => '显示你的最新的短贴',
-	'thewire:num' => '显示帖子数量',
-	'thewire:moreposts' => '更多短贴',
+	'thewire:widget:desc' => '显示你的最新推文',
+	'thewire:num' => '显示推文数量',
+	'thewire:moreposts' => '更多推文',
 
 	/**
 	 * Status messages
 	 */
-	'thewire:posted' => "你的信息成功贴出",
-	'thewire:deleted' => "短贴被成功删除",
+	'thewire:posted' => "你的信息已经成功贴出",
+	'thewire:deleted' => "推文被成功删除",
 	'thewire:blank' => "发布帖子之前，你需要输入文本内容",
-	'thewire:notfound' => "我们未能发现指定的短贴",
-	'thewire:notsaved' => "我们不能保存这个短贴",
-	'thewire:notdeleted' => "我们不能删除这个短贴",
+	'thewire:notfound' => "未能发现指定推文",
+	'thewire:notsaved' => "不能保存这个推文",
+	'thewire:notdeleted' => "不能删除这个推文",
 
 	/**
 	 * Notifications
 	 */
 	'thewire:notify:summary' => '新帖: %s',
 	'thewire:notify:subject' => "%s的新帖",
-	'thewire:notify:reply' => '%s 回应 %s 在帖子:',
-	'thewire:notify:post' => '%s 发布帖子:',
+	'thewire:notify:reply' => '%s 回应 %s 在推文:',
+	'thewire:notify:post' => '%s发布推文:',
 	'thewire:notify:footer' => "查看并且回复:\n%s",
 
 	/**
