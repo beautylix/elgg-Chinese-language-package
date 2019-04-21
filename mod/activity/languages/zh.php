2ffcfda6ed5b94c810e26a21f72c17975ba695ac
beautylix <beautylix@126.com>
2019-04-21 17:03:00 +0800

    new
    new added

---
+++ mod/activity/languages/zh.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+	'widgets:river_widget:name' => "活动",
+	'widgets:river_widget:description' => "显示最新活动",
+	'widgets:river_widget:type' => "活动类型",
+	'widgets:river_widget:friends' => '好友的活动',
+	'widgets:river_widget:all' => '全部活动',
+
+	'widgets:group_activity:name' => '小组活动',
+	'widgets:group_activity:description' => '查看你所在小组的活动',
+	'widgets:group_activity:edit:select' => '选择一个小组',
+	'widgets:group_activity:content:noactivity' => '此小组暂无活动',
+	'widgets:group_activity:content:noselect' => '编辑此组件选择一个小组',
+	
+	'collection:river:group' => "小组活动",
+	'groups:tool:activity' => '开启小组活动,
+	
+);
